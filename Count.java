01ae03ce307f082c579d7670d2db865cc42e4d28
Gayatri Chippawar <gayatrichippawar@gmail.com>
2026-03-09 08:21:50 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,37 @@
+package Binary_Tree;
+
+public class Count {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+        public static int countNodes(Node root){
+            if(root==null){
+                return 0;
+            }
+            int lhCount = countNodes(root.left);
+            int rhCount = countNodes(root.right);
+
+            return lhCount+rhCount+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(root.countNodes(root));
+    }
+}
